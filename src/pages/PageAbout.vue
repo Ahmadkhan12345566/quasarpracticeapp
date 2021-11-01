1653cc242f7c0534fd4f32bb3c4a9a37ba6e6e96
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2021-11-01 10:41:54 +0500

    home page setup

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page  class="q-pa-lg">
+    <h3>About Qwitter</h3>
+    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to
+      demonstrate the visual form of a document or a typeface without relying on meaningful content.
+      Lorem ipsum may be used as a placeholder before final copy is available.</p>
+    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to
+      demonstrate the visual form of a document or a typeface without relying on meaningful content.
+      Lorem ipsum may be used as a placeholder before final copy is available.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageAbout'
+})
+</script>
