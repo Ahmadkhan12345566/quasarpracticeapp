4e57f4ca68cf729363f6cd42a35e1bd74796a3e1
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2021-11-03 15:24:35 +0500

    about page setup and home page setup

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,12 +1,14 @@
 <template>
   <q-page  class="q-pa-lg">
-    <h3>About Qwitter</h3>
-    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to
-      demonstrate the visual form of a document or a typeface without relying on meaningful content.
-      Lorem ipsum may be used as a placeholder before final copy is available.</p>
-    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to
-      demonstrate the visual form of a document or a typeface without relying on meaningful content.
-      Lorem ipsum may be used as a placeholder before final copy is available.</p>
+    <h3 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h3>
+   <div class="text-body1">
+     <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to
+       demonstrate the visual form of a document or a typeface without relying on meaningful content.
+       Lorem ipsum may be used as a placeholder before final copy is available.</p>
+     <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to
+       demonstrate the visual form of a document or a typeface without relying on meaningful content.
+       Lorem ipsum may be used as a placeholder before final copy is available.</p>
+   </div>
   </q-page>
 </template>
 
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,5 +1,31 @@
 <template>
   <q-page >
+    <div class="q-py-lg q-px-md">
+      <q-input bottom-slots
+               v-model="newQweetContent"  placeholder="What's happening?" counter maxlength="280" :dense="dense">
+
+        <template v-slot:before>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+          </q-avatar>
+        </template>
+
+        <template v-slot:append>
+          <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
+          <q-icon name="schedule" />
+        </template>
+
+        <template v-slot:hint>
+          Field hint
+        </template>
+
+        <template v-slot:after>
+          <q-btn round dense flat icon="send" />
+        </template>
+
+      </q-input>
+    </div>
+
   </q-page>
 </template>
 
