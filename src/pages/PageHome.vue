85d031d4c7909a0401d1c492b21df13f96f4dc1d
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2021-11-04 01:27:08 +0500

    post area setup

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -19,6 +19,45 @@
     </div>
 
     <q-separator class="divider" color="grey-2" size="10px"/>
+
+
+    <q-list>
+
+
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section class="text-subtitle1">
+          <q-item-label >
+            <strong>
+            Arslan Ahmad
+            </strong>
+            <span class="text-grey-7"> @ahmadkhan</span>
+          </q-item-label>
+          <q-item-label class="qweet-content text-body1" >
+            I'll be in your neighborhood doing errands this weekend.
+                  Do you want to grab brunch?
+          </q-item-label>
+          <div class="row">
+            <q-btn color="grey" icon="gift">
+
+            </q-btn>
+
+          </div>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+
+
+    </q-list>
+
   </q-page>
 </template>
 
@@ -45,5 +84,7 @@ export default defineComponent({
   border-bottom: 1px solid
   border-color: $grey-4
 
+.qweet-content
+  white-space: pre-line
 
 </style>
