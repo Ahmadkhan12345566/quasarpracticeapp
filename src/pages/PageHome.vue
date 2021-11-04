f3f8a38afa523b98bae4c73824187c02908dfffd
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2021-11-04 11:03:35 +0500

    front end final touch

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,5 +1,6 @@
 <template>
-  <q-page >
+  <q-page class="relative-position" >
+    <q-scroll-area class="absolute fullscreen">
     <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
      <div class="col">
        <q-input class="new-qweet" bottom-slots
@@ -14,17 +15,22 @@
        </q-input>
      </div>
       <div class="col col-shrink">
-        <q-btn class="q-mb-lg" :disable="!newQweetContent" unelevated rounded color="primary" label="Qweet" no-caps  />
+        <q-btn @click="addNewQweet" class="q-mb-lg" :disable="!newQweetContent" unelevated rounded color="primary" label="Qweet" no-caps  />
       </div>
     </div>
 
     <q-separator class="divider" color="grey-2" size="10px"/>
 
 
-    <q-list>
+    <q-list separator>
+      <transition-group
+        appear
+        enter-active-class="animated fadeIn"
+        leave-active-class="animated fadeOut"
+         class="slow"
+      >
 
-
-      <q-item class="q-py-md">
+      <q-item v-for="qweet in qweets" :key="qweet.date" class="qweet q-py-md">
         <q-item-section avatar top>
           <q-avatar>
             <img src="https://cdn.quasar.dev/img/avatar2.jpg">
@@ -36,39 +42,69 @@
             <strong>
             Arslan Ahmad
             </strong>
-            <span class="text-grey-7"> @ahmadkhan</span>
+            <span class="text-grey-7"> @ahmadkhan &bull;  <br class="lt-md"> {{ relativeDate(qweet.date) }} </span>
           </q-item-label>
           <q-item-label class="qweet-content text-body1" >
-            I'll be in your neighborhood doing errands this weekend.
-                  Do you want to grab brunch?
+           {{ qweet.content }}
           </q-item-label>
-          <div class="row">
-            <q-btn color="grey" icon="gift">
-
-            </q-btn>
-
+          <div class="qweet-icons row justify-between q-mt-sm">
+            <q-btn flat round color="primary" icon="far fa-comment" size="sm" />
+            <q-btn flat round color="primary" icon="fas fa-retweet" size="sm" />
+            <q-btn flat round color="primary" icon="far fa-heart" size="sm" />
+            <q-btn @click="deleteQweet(qweet)" flat round color="primary" icon="fas fa-trash" size="sm" />
           </div>
         </q-item-section>
 
-        <q-item-section side top>
-          1 min ago
-        </q-item-section>
       </q-item>
+      </transition-group>
 
 
     </q-list>
 
+    </q-scroll-area>
   </q-page>
 </template>
 
 <script>
+
 import { defineComponent } from 'vue';
+import { formatDistance } from 'date-fns'
 
 export default defineComponent({
   name: 'PageHome',
   data(){
     return{
-      newQweetContent: ''
+      newQweetContent: '',
+      qweets:[
+        {
+          content:'I ll be in your neighborhood doing errands this weekend.Do you want to grab brunch?',
+          date:1635971999205
+        },
+        {
+          content:'Do you want to grab brunch?',
+          date:1635971970244
+        }
+      ]
+
+    }
+  },
+  methods:{
+    relativeDate(value){
+      return formatDistance(value, new Date())
+    },
+    addNewQweet(){
+      console.log("Add new qweet")
+      let Qweet= {
+        content:this.newQweetContent,
+        date:Date.now()
+      }
+      this.qweets.unshift(Qweet)
+      this.newQweetContent = ' '
+    },
+    deleteQweet(qweet){
+      let dateToDelete = qweet.date
+      let index = this.qweets.findIndex(qweet => qweet.date === dateToDelete)
+      this.qweets.splice(index,1)
     }
   }
 })
@@ -87,4 +123,9 @@ export default defineComponent({
 .qweet-content
   white-space: pre-line
 
+.qweet-icons
+  margin-left: -5px
+
+.qweet:not(:first-child)
+  border-top: 1px solid rgba(0,0,0,0.12)
 </style>
